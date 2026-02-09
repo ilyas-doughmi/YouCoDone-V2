39452146cf6197d5030743fa6956d1a2e7642fbe
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-09 19:00:21 +0100

    feat: Implement reservation time validation in store method of ReservationController

--- web/app/Http/Controllers/ReservationController.php
+++ web/app/Http/Controllers/ReservationController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreReservationRequest;
+use App\Models\Horaire;
+use App\Models\restaurant;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -14,6 +17,23 @@ class ReservationController extends Controller
 
     public function store(StoreReservationRequest $request)
     {
-        // soon :)
+        $data = $request->validated();
+        $restaurant = restaurant::findOrFail($data['restaurant_id']);
+        $date = Carbon::parse($data['date']);
+        $jourActual = $date->format('l');
+
+        $horaire = Horaire::where('restaurantId', $restaurant->id)
+                            ->where('jour', $jourActual)
+                            ->first();
+
+        if(!$horaire || $horaire->status == 'fermé') {
+            return back()->withErrors(['date' => "restaurant is closed $jourActual "])->withInput();
+        }
+
+        if($data['heure'] < $horaire->heure_ouverture || $data['heure'] > $horaire->heure_ouverture)
+        {
+            return back()->withErrors(['heure' => "reservation time need to be from {$horaire->heure_ouverture} à {$horaire->heure_fermeture}"]);
+        } 
+
     }
 }
