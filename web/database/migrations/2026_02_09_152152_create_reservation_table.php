94b5a37de13389f798dc32f8d37ba87d7cb2ed50
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-09 16:35:40 +0100

    feat: Create migration for reservation table with user and restaurant relationships

---
+++ web/database/migrations/2026_02_09_152152_create_reservation_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('restaurant_id')->constrained('restaurant')->onDelete('cascade')->index();
+            $table->date('date')->index();
+            $table->time('heure');
+            $table->integer('nombre_personnes');
+            $table->string('status')->default('en_attente'); // en_attente - confirme - refuse
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation');
+    }
+};
