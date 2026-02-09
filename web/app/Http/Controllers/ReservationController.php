cc069dd74628c22173d5d00942f12ef9a21f200e
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-09 16:50:29 +0100

    feat: Implement store method in ReservationController for handling reservations

--- web/app/Http/Controllers/ReservationController.php
+++ web/app/Http/Controllers/ReservationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreReservationRequest;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -9,5 +10,10 @@ class ReservationController extends Controller
     public function index()
     {
         return view('restaurateur.reservations.index');
+    }
+
+    public function store(StoreReservationRequest $request)
+    {
+        // soon :)
     }
 }
