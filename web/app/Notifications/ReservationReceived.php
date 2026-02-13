5b04105a786a305deee239c9c29e5c20dd4033a3
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-13 10:38:19 +0100

    feat: add ReservationReceived notification for new reservations

---
+++ web/app/Notifications/ReservationReceived.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ReservationReceived extends Notification
+{
+    use Queueable;
+
+    protected $reservation;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('Vous avez reçu une nouvelle réservation.')
+                    ->action('Voir la réservation', url('/reservations'))
+                    ->line('Merci d\'utiliser notre application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'reservation_id' => $this->reservation->id,
+            'client_name' => $this->reservation->user->name,
+            'date' => $this->reservation->date,
+            'heure' => $this->reservation->heure,
+            'personnes' => $this->reservation->nombre_personnes,
+            'message' => 'Nouvelle réservation de ' . $this->reservation->user->name
+        ];
+    }
+}
